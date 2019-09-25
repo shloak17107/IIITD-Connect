4c6575e08bbb8ce62c97721eade4af643d0e9a4e
bhrigu12 <35344501+bhrigu12@users.noreply.github.com>
2019-09-25 19:28:06 +0530

    Login page + About page

--- app/src/main/java/com/example/iiitdconnect/About_fragment.java
+++ app/src/main/java/com/example/iiitdconnect/About_fragment.java
@@ -7,15 +7,10 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
-import com.google.android.gms.common.SignInButton;
+public class About_fragment extends Fragment {
 
-public class Login_page_fragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.login_page_fragment_layout,container,false);
         return view;
-//        (SignInButton) view.findViewById(R.id.sign_in_button).setOnClickListener();
     }
-//    SignInButton signInButton = (SignInButton) view.findViewById(R.id.sign_in_button);
-//    signInButton.ButtonSize(short);
-
 }
