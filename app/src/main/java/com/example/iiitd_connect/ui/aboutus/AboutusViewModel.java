5bc4b8bc263bb46a4438b4002595bf3575423d3e
anshuman1117 <anshuman17025@iiitd.ac.in>
2019-09-25 12:07:36 +0530

    Initial Main UI, Landing page

---
+++ app/src/main/java/com/example/iiitd_connect/ui/aboutus/AboutusViewModel.java
@@ -0,0 +1,19 @@
+package com.example.iiitd_connect.ui.aboutus;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class AboutusViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public AboutusViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is about us fragment constructor, in AboutusViewModel.java");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
